5c5ae130ad1def303c0d1de39461b1741b1d7ba6
WimHambrouck-EhB <wim.hambrouck@ehb.be>
2023-06-14 03:44:31 +0200

    Koppel personeel

--- ExamenCodeAlong/Controllers/ProjectenController.cs
+++ ExamenCodeAlong/Controllers/ProjectenController.cs
@@ -22,9 +22,9 @@ namespace ExamenCodeAlong.Controllers
         // GET: Projecten
         public async Task<IActionResult> Index()
         {
-              return _context.Projecten != null ? 
-                          View(await _context.Projecten.Include(p => p.PersoneelslidProjecten).ThenInclude(pp => pp.Personeelslid).ToListAsync()):
-                          Problem("Entity set 'EhelpBContext.Projecten'  is null.");
+            return _context.Projecten != null ?
+                        View(await _context.Projecten.Include(p => p.PersoneelslidProjecten).ThenInclude(pp => pp.Personeelslid).ToListAsync()) :
+                        Problem("Entity set 'EhelpBContext.Projecten'  is null.");
         }
 
 
@@ -48,11 +48,81 @@ namespace ExamenCodeAlong.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(project);
+
+
+        }
+
+        public async Task<IActionResult> Koppelpersoneel(KoppelpersoneelViewModel? viewModel)
+        {
+            viewModel ??= new KoppelpersoneelViewModel();
+
+            if (viewModel?.Project != null)
+            {
+                var personeelInProject = _context.PersoneelslidProject.Where(pp => pp.ProjectId == viewModel.Project.ProjectNaam).Include(pp => pp.Personeelslid);
+                var personeelNietInProject = await _context.Personeelsleden.Where(p => !personeelInProject.Any(pp => pp.PersoneelslidId == p.Id)).ToListAsync();
+
+                foreach (var project in personeelInProject)
+                {
+                    viewModel.Koppelingen.Add(project.Personeelslid, true);
+                }
+
+                foreach (var project in personeelNietInProject)
+                {
+                    viewModel.Koppelingen.Add(project, false);
+                }
+            }
+
+            ViewData["Projecten"] = new SelectList(_context.Projecten, "ProjectNaam", "ProjectNaam");
+
+            return View(viewModel);
+        }
+
+        [HttpPost, ActionName("Koppelpersoneel")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> KoppelpersoneelVerzenden(KoppelpersoneelViewModel? viewModel)
+        {
+            if (viewModel == null || viewModel.Project == null)
+            {
+                return NotFound();
+            }
+
+            var personeelslidProjecten = _context.PersoneelslidProject.Where(pp => pp.ProjectId == viewModel.Project.ProjectNaam);
+
+            foreach (var item in viewModel.Koppelingen)
+            {
+                PersoneelslidProject? personeelslid = await personeelslidProjecten.SingleOrDefaultAsync(pp => pp.PersoneelslidId == item.Key.Id);
+
+                if (item.Value)
+                {
+                    if (personeelslid == null)
+                    {
+                        personeelslid = new PersoneelslidProject
+                        {
+                            PersoneelslidId = item.Key.Id,
+                            ProjectId = viewModel.Project.ProjectNaam
+                        };
+
+                        _context.Add(personeelslid);
+                    }
+                }
+                else
+                {
+                    if (personeelslid != null)
+                    {
+                        _context.Remove(personeelslid);
+                    }
+                }
+            }
+
+            await _context.SaveChangesAsync();
+
+
+            return RedirectToAction(nameof(Index));
         }
 
         private bool ProjectExists(string id)
         {
-          return (_context.Projecten?.Any(e => e.ProjectNaam == id)).GetValueOrDefault();
+            return (_context.Projecten?.Any(e => e.ProjectNaam == id)).GetValueOrDefault();
         }
     }
 }
---
+++ ExamenCodeAlong/Models/KoppelpersoneelViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ExamenCodeAlong.Models
+{
+    public class KoppelpersoneelViewModel
+    {
+        public Project? Project { get; set; }
+
+        public Dictionary<Personeelslid, bool> Koppelingen { get; set; } = new Dictionary<Personeelslid, bool>();
+    }
+}
